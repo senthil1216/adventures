f7e89f73fe478b28c26299194fd8df551dd6979e
senthil1216 <senthil.sivasubraman@gusto.com>
2022-12-03 20:33:43 -0800

    updated 2021 day1 solutions

--- app/src/main/java/adventures/App.java
+++ app/src/main/java/adventures/App.java
@@ -5,6 +5,10 @@ public class App {
         String runP = args[0];
         // String runP = "202201";
         switch (runP) {
+            case "202101":
+                new Puzzle202101().part1();
+                new Puzzle202101().part2();
+                break;
             case "202201":
                 new Puzzle202201().part1();
                 new Puzzle202201().part2();
---
+++ app/src/main/java/adventures/Puzzle202101.java
@@ -0,0 +1,39 @@
+
+package adventures;
+
+import java.util.*;
+
+class Puzzle202101 {
+    public void part1() {
+        List<String> data = Helpers.loadFile("20210101.txt");
+        int prev = Integer.parseInt(data.get(0));
+        int maxValue = 0;
+        int curr = 0;
+        for (int i = 1; i < data.size(); i++) {
+            String d = data.get(i);
+            curr = Integer.parseInt(d);
+            if (curr > prev) {
+                maxValue++;
+            }
+            prev = curr;
+        }
+        System.out.println(maxValue);
+    }
+
+    public void part2() {
+        List<String> data = Helpers.loadFile("20210101.txt");
+        int prevSum = 0;
+        int count = 0;
+        int currSum = 0;
+        for (int i = 0; i <= data.size() - 3; i++) {
+            currSum = Integer.parseInt(data.get(i)) + Integer.parseInt(data.get(i + 1))
+                    + Integer.parseInt(data.get(i + 2));
+            if (prevSum > 0 && currSum > prevSum) {
+                count++;
+            }
+            prevSum = currSum;
+        }
+        System.out.println(count);
+    }
+
+}
