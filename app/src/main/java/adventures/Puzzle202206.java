7d5955596498f3b04dc93c35138df06aac5f6878
senthil1216 <senthil.sivasubraman@gusto.com>
2022-12-06 00:14:09 -0800

    updated

--- app/src/main/java/adventures/Puzzle202206.java
+++ app/src/main/java/adventures/Puzzle202206.java
@@ -9,33 +9,24 @@ class Puzzle202206 {
     public void part1() {
         String data = Helpers.loadFileAsString("202206.txt");
         boolean found = false;
-        for (int i = 0; i < data.length() - 4; i++) {
-            if (found) {
-                continue;
-            }
+        int i = 0;
+        while (!found) {
             List<Character> chars = Chars.asList(data.substring(i, i + 4).toCharArray());
-            List<Character> distinct = chars.stream().distinct().collect(Collectors.toList());
-            if (chars.size() == distinct.size()) {
-                System.out.println(i + 4);
-                found = true;
-            }
+            found = chars.stream().distinct().collect(Collectors.toList()).size() == 4;
+            i++;
         }
+        System.out.println(i + 3);
     }
 
     public void part2() {
         String data = Helpers.loadFileAsString("202206.txt");
         boolean found = false;
-        for (int i = 0; i < data.length() - 14; i++) {
-            if (found) {
-                continue;
-            }
-            List<Character> chars = Chars.asList(data.substring(i, i +
-                    14).toCharArray());
-            List<Character> distinct = chars.stream().distinct().collect(Collectors.toList());
-            if (chars.size() == distinct.size()) {
-                System.out.println(i + 14);
-                found = true;
-            }
+        int i = 0;
+        while (!found) {
+            List<Character> chars = Chars.asList(data.substring(i, i + 14).toCharArray());
+            found = chars.stream().distinct().collect(Collectors.toList()).size() == 14;
+            i++;
         }
+        System.out.println(i + 13);
     }
 }
