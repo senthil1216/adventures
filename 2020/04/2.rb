0e56d110d1ec7c18f2f4b901895ef3761b73082a
senthil sivasubramanian <senthil1216@gmail.com>
2024-06-09 14:23:23 -0700

    Solved Day 4 part2

--- 2020/04/2.rb
+++ 2020/04/2.rb
@@ -1,24 +1,29 @@
-#! /usr/bin/env ruby
+#!/usr/bin/env ruby
 
-def find_trees(lines, row_step, col_step)
-  rowCount, colCount = lines.size, lines[0].size
-  count, r,c = 0,0,0
-  while r < rowCount
-    count += 1 if lines[r][c] == "#"
-    r += row_step
-    c += col_step
-    c = (c - colCount) if c >= colCount
+def is_valid_height(passport)
+  ht_value = passport["hgt"]
+  (ht_value&.end_with?("in") && (59..76).include?(ht_value.to_i)) || 
+  (ht_value&.end_with?("cm") && (150..193).include?(ht_value.to_i))
+end
+
+def check(passport)
+  [
+    (1920..2002).include?(passport["byr"]&.to_i),
+    (2010..2020).include?(passport["iyr"]&.to_i),
+    (2020..2030).include?(passport["eyr"]&.to_i),
+    is_valid_height(passport),
+    /^#[0-9a-f]{6}$/.match?(passport["hcl"]),
+    ["amb","blu","brn","gry","grn","hzl","oth"].include?(passport["ecl"]),
+    /^[0-9]{9}$/.match?(passport["pid"])
+  ].all?(true)
+end
+
+count = 0
+ARGF.read.split("\n\n").each do |each_line|
+  passport = {}
+  each_line.split(" ").each do |k|
+    passport[k.split(":")[0].strip] = k.split(":")[1].strip
   end
-  count
+  count+=1 if check(passport)
 end
-
-lines = []
-ARGF.each_line do |line|
-  lines << line.strip
-end
-puts find_trees(lines, 1, 1) * 
-  find_trees(lines, 1, 3) * 
-  find_trees(lines, 1, 5) * 
-  find_trees(lines, 1, 7) * 
-  find_trees(lines, 2, 1)
-
+puts count
