8f6a025c662f3a48ac7bfe6c702962c4a72f27d1
senthil1216 <senthil.sivasubraman@gusto.com>
2024-06-17 13:36:58 -0700

    simplified the part1 solution

--- 2020/09/1.rb
+++ 2020/09/1.rb
@@ -3,22 +3,10 @@
 input = ARGF.read.split("\n").map(&:to_i)
 LIMIT = 25
 
-invalid_num = 0
-
 (LIMIT..input.size-1).step(1) do |id|
-  found_valid_sum = false
-  numSet = Set.new
   invalid_num = input[id]
-  (id-1).downto(id-LIMIT).each do |prev_id|
-    numSet.add(input[id] - input[prev_id])
+  if !input.slice(id-LIMIT, LIMIT).combination(2).map(&:sum).include?(input[id])
+    pp invalid_num
+    break
   end
-  (id-1).downto(id-LIMIT).each do |prev_id|
-    if numSet.include?(input[prev_id])
-      found_valid_sum = true
-      break
-    end
-  end
-  break if !found_valid_sum
 end
-
-pp invalid_num
