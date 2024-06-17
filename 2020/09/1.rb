88ba3f213d3c3a910c68b89f0e6a5bdcff1e9f36
senthil1216 <senthil.sivasubraman@gusto.com>
2024-06-17 14:00:04 -0700

    solved the problem with combinations

--- 2020/09/1.rb
+++ 2020/09/1.rb
@@ -5,7 +5,7 @@ LIMIT = 25
 
 (LIMIT..input.size-1).step(1) do |id|
   invalid_num = input[id]
-  if !input.slice(id-LIMIT, LIMIT).combination(2).map(&:sum).include?(input[id])
+  if !input.slice(id-LIMIT, LIMIT).combination(2).map(&:sum).include?(invalid_num)
     pp invalid_num
     break
   end
